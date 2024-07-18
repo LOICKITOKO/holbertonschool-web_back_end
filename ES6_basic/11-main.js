52e81d9b0b4769889f210fea641323bb96e7c170
LOICKITOKO <loicassaelkm@gmail.com>
2024-07-18 21:18:41 +0200

    task11

---
+++ ES6_basic/11-main.js
@@ -0,0 +1,10 @@
+export default function createEmployeesObject(departmentName, employees) {
+  return {
+    [departmentName]: employees
+  };
+}
+export default function createEmployeesObject(departmentName, employees) {
+  return {
+    [departmentName]: employees
+  };
+}
