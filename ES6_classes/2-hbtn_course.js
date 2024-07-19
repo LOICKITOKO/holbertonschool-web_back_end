851bee2de76cfc4a07649d93b63cb22b67c310bb
LOICKITOKO <loicassaelkm@gmail.com>
2024-07-19 21:47:22 +0200

    review all

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,58 +1,47 @@
+// Holberton Course
 export default class HolbertonCourse {
-  constructor(name, length, students) {
-    this._name = this._validateString(name, 'Name');
-    this._length = this._validateNumber(length, 'Length');
-    this._students = this._validateStudents(students);
+  constructor(name = '', length = 0, students = []) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  // Getter and Setter for name
   get name() {
     return this._name;
   }
 
   set name(value) {
-    this._name = this._validateString(value, 'Name');
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
   }
 
-  // Getter and Setter for length
   get length() {
     return this._length;
   }
 
   set length(value) {
-    this._length = this._validateNumber(value, 'Length');
+    if (typeof value !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = value;
   }
 
-  // Getter and Setter for students
   get students() {
     return this._students;
   }
 
-  set students(value) {
-    this._students = this._validateStudents(value);
-  }
-
-  // Private method to validate string type
-  _validateString(value, attributeName) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${attributeName} must be a string`);
-    }
-    return value;
-  }
-
-  // Private method to validate number type
-  _validateNumber(value, attributeName) {
-    if (typeof value !== 'number') {
-      throw new TypeError(`${attributeName} must be a number`);
-    }
-    return value;
-  }
-
-  // Private method to validate students array
-  _validateStudents(value) {
-    if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
+  set students(currStudents) {
+    if (typeof currStudents === 'object') {
+      for (const student in currStudents) {
+        if (typeof student !== 'string') {
+          throw new TypeError('Students must be an array of strings');
+        }
+      }
+    } else {
       throw new TypeError('Students must be an array of strings');
     }
-    return value;
+    this._students = currStudents;
   }
 }
