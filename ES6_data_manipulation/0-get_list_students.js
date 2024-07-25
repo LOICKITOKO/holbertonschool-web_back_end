e36c86e6ee252ed2b44bf2f6ee6668320f44c60d
LOICKITOKO <loicassaelkm@gmail.com>
2024-07-25 14:26:46 +0200

    task0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,7 @@
-// Get List Students
-
-const getListStudents = () => {
-    const students = [
-      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-      { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    ];
-  
-    return students;
-  };
-  
-  export default getListStudents;
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+}
