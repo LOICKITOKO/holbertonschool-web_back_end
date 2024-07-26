62f4e810ba68256c00f06246530cbf897b418964
LOICKITOKO <loicassaelkm@gmail.com>
2024-07-26 15:52:37 +0200

    review task6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,13 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason
-  })));
+    const signUpPromise = signUpUser(firstName, lastName);
+    const uploadPhotoPromise = uploadPhoto(fileName);
+
+    return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+        .then((results) => results.map(result => ({
+            status: result.status,
+            value: result.status === 'fulfilled' ? result.value : result.reason,
+        })));
 }
