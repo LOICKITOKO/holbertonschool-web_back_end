e836349dac0f5c352bbe5f9212f01bb907165c29
LOICKITOKO <loicassaelkm@gmail.com>
2024-07-26 15:54:55 +0200

    task6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,13 +1,18 @@
+// Import the necessary functions
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-    const signUpPromise = signUpUser(firstName, lastName);
-    const uploadPhotoPromise = uploadPhoto(fileName);
+// Define and export the handleProfileSignup function
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  // Use Promise.allSettled to handle both promises
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ]);
 
-    return Promise.allSettled([signUpPromise, uploadPhotoPromise])
-        .then((results) => results.map(result => ({
-            status: result.status,
-            value: result.status === 'fulfilled' ? result.value : result.reason,
-        })));
+  // Format the results as required
+  return results.map(result => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  }));
 }
